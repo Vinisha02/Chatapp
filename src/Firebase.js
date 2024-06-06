87cdee990b5e7d640d38a01dd55fe0d3b9126025
Vinisha02 <vinisha.choudhary02@gmail.com>
2024-06-06 18:00:31 +0530

    ChatApp

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYN__X8Ne1VqU3PELMK-MIzYOXkFWRbN0",
+  authDomain: "chatapp-a9970.firebaseapp.com",
+  projectId: "chatapp-a9970",
+  storageBucket: "chatapp-a9970.appspot.com",
+  messagingSenderId: "78816976706",
+  appId: "1:78816976706:web:6d24635ac7228d8de932db",
+};
+
+
+export const app = initializeApp(firebaseConfig);
